03aa112c1ffd67561f3b145401e18450c42039d2
Danny Locke <dlocke@mac.com>
2016-11-14 11:27:46 -0600

    IronYardDay1

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Person p = new Person();
+        p.name = "Alice";
+        p.age = 20;
+        p.isAlive = true;
+
+        System.out.println(p.name);
+    }
+}
